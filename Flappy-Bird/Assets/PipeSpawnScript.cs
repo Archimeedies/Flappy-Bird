2149bfb5c23621f8caf4448fb65f533193c1bc44
Eli <elijahhiraka15@gmail.com>
2024-06-08 03:42:05 +1200

    Added background, game over and scores

--- Flappy-Bird/Assets/PipeSpawnScript.cs
+++ Flappy-Bird/Assets/PipeSpawnScript.cs
@@ -7,7 +7,7 @@ public class PipeSpawnScript : MonoBehaviour
     public GameObject pipe;
     public float spawnRate = 2;
     private float timer = 0;
-    public float pipeOffset = 10;
+    public float pipeOffset = 4;
 
     // Start is called before the first frame update
     void Start()
